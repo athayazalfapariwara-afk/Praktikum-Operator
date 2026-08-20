9ec115ac6b82b0ffd4a7250f751558fa9acdf6b0
Zalfa <athayazalfapariwara@gmail.com>
2026-08-20 15:11:45 +0700

    Add files via upload

---
+++ praktikum1_aritmatika.js
@@ -0,0 +1,15 @@
+let panjang = 8;
+let lebar = 5;
+
+//luas persegi panjang
+let luas = panjang * lebar
+
+//keliling persegi panjang
+let keliling = (panjang + lebar) * 2
+
+//sisa bagi luas dengan 3
+let sisa = luas % 3
+
+console.log('Luas:', luas)
+console.log('Keliling:', keliling)
+console.log('Sisa:', sisa)
---
+++ praktikum2_penugasan.js
@@ -0,0 +1,12 @@
+let saldo = 50000;
+
+//andi menabung 20000
+saldo+=20000
+
+//andi menarik uang 15000
+saldo-=15000
+
+//saldo kali 2 karena bonus
+saldo*=2
+
+console.log('Saldo akhir:', saldo)
